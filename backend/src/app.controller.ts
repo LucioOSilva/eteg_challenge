f3cf8cadf18d1e5b6ad44f46182ed16ca558557e
lucioosilva <lucioosilva00@gmail.com>
2026-06-24 17:59:58 -0300

    feat: updates api health check controller base path

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/api/health-check')
+  @Get('/health-check')
   getHello(): string {
     return this.appService.health();
   }
