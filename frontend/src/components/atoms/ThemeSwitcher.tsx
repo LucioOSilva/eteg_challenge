9a37a082ca5ed8773f26878ab895640768b6f620
lucioosilva <lucioosilva00@gmail.com>
2026-06-25 20:39:27 -0300

    feat: Creates use theme switch

---
+++ frontend/src/components/atoms/ThemeSwitcher.tsx
@@ -0,0 +1,12 @@
+import { useTheme } from '@/hooks';
+import { THEMES } from '@/components/context';
+
+export const ThemeSwitcher = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <select value={theme} onChange={e => setTheme(e.target.value as ThemeName)}>
+      {THEMES.map(t => <option key={t} value={t}>{t}</option>)}
+    </select>
+  );
+};
--- frontend/src/components/template/MainTemplate/MainTemplate.tsx
+++ frontend/src/components/template/MainTemplate/MainTemplate.tsx
@@ -1,8 +1,14 @@
 import { type FC } from "react";
+import { ThemeSwitcher } from "@/components/atoms";
 
 export const MainTemplate: FC<MainTemplateProps> = ({ children }) => {
   return (
-    <div className="">{children}</div>
+    <div className="bg-background">
+      <ThemeSwitcher />
+      <div className="max-w-[1280px] mx-auto bg-input h-screen">
+        {children}
+      </div>
+    </div>
   );
 };
 
