d17634dba0b89de07249716cf227457e329fa3ed
lucioosilva <lucioosilva00@gmail.com>
2026-06-25 22:21:22 -0300

    chore: updates exports from molecules

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -1,3 +1,6 @@
-import { ThemeSwitcher } from '../molecules/ThemeSwitcher/ThemeSwitcher';
+import { ColorPicker } from "../molecules/ColorPicker/ColorPicker";
+import { InputField } from "../molecules/InputField/InputField";
+import { TextareaField } from "../molecules/TextareaField/TextareaField";
+import { ThemeSwitcher } from "../molecules/ThemeSwitcher/ThemeSwitcher";
 
-export { ThemeSwitcher };
+export { ColorPicker, InputField, TextareaField, ThemeSwitcher };
