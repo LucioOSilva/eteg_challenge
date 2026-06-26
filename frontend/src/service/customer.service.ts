ca5ae8988a46c6374f51bcad3e51ef6ae15d0a89
lucioosilva <lucioosilva00@gmail.com>
2026-06-26 01:07:07 -0300

    chore: improve service

--- frontend/src/service/customer.service.ts
+++ frontend/src/service/customer.service.ts
@@ -1,31 +1,10 @@
 import { request } from './http';
 
-export const listCustomersPaginated = async (page = 1, limit = 10): Promise<RestResponse<PaginatedType<Customer>> | undefined> => {
-  try {
-    return await request<RestResponse<PaginatedType<Customer>>>(
-      'GET',
-      `/api/users/list-paginated?page=${page}&limit=${limit}`
-    );
-  } catch (error) {
-    console.error('Erro ao buscar clientes');
-    return undefined;
-  }
-};
+export const listCustomersPaginated = (page = 1,limit = 10): Promise<RestResponse<PaginatedType<Customer>>> =>
+  request<RestResponse<PaginatedType<Customer>>>('GET', `/api/users/list-paginated?page=${page}&limit=${limit}`);
 
-export const createCustomer = async (payload: CreateCustomerPayload): Promise<RestResponse<Customer> | undefined> => {
-  try {
-    return await request<RestResponse<Customer>>('POST', '/api/users/create', payload);
-  } catch (error) {
-    console.error('Erro ao criar cliente');
-    return undefined;
-  }
-};
+export const createCustomer = (payload: CreateCustomerPayload): Promise<Customer> =>
+  request<Customer>('POST', '/api/users/create', payload);
 
-export const updateCustomerColor = async (payload: UpdateCustomerColorPayload): Promise<RestResponse<Customer> | undefined> => {
-  try {
-    return await request<RestResponse<Customer>>('PATCH', '/api/users/update-user-color', payload);
-  } catch (error) {
-    console.error('Erro ao atualizar cor do cliente');
-    return undefined;
-  }
-};
+export const updateCustomerColor = (payload: UpdateCustomerColorPayload): Promise<Customer> =>
+  request<Customer>('PATCH', '/api/users/update-user-color', payload);
